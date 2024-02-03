e3a35e82ffd228afc9cf64e100a1a7d7cff8bc56
Luis Solis <37953927+solisgb@users.noreply.github.com>
2024-02-03 19:00:10 +0100

    Add files via upload

--- no_sleep.ps1
+++ no_sleep.ps1
@@ -16,22 +16,25 @@ if (-not $WindowsPrincipal.IsInRole($AdminRole))
 
 # Step 1. Disable sleep and hibernation
 & powercfg -h off
-& powercfg -s off
+& powercfg -change -standby-timeout-ac 0
+& powercfg -change -standby-timeout-dc 0
 
 # Step 2. Open the window. When the window is closed, execute step 3.
 Add-Type -AssemblyName System.Windows.Forms
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Notification'
 $form.TopMost = $true
+$form.Size = New-Object System.Drawing.Size(350, 100)
 
 $label = New-Object System.Windows.Forms.Label
 $label.Text = 'PC is active until this window is open'
 $label.AutoSize = $true
-$label.Font = New-Object System.Drawing.Font("Arial",16,[System.Drawing.FontStyle]::Bold)
+$label.Font = New-Object System.Drawing.Font("Arial",12,[System.Drawing.FontStyle]::Bold)
 $form.Controls.Add($label)
 
 $form.ShowDialog() | Out-Null
 
 # Step 3. Re-enable sleep and hibernation
 & powercfg -h on
-& powercfg -s on
+& powercfg -change -standby-timeout-ac 20
+& powercfg -change -standby-timeout-dc 20
